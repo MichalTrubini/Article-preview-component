1e57e0164bc42659e41597b7c0eaaf5ecaee6f17
Michal Trubini <mchl.trubini@gmail.com>
2021-11-27 19:36:42 +0100

    desktop version re-factored as per figma, js added

--- js/script.js
+++ js/script.js
@@ -1,11 +1,26 @@
-let share = document.querySelectorAll('.share-button');
+let shareButton = document.querySelectorAll('.share-button');
+let share = document.querySelector('.content__wrapper-image');
+let arrow = document.querySelector('.content__share-button');
+let shareBlock = document.querySelector('.share-block');
 
-share.forEach(shareSelected);
+share.addEventListener('mouseenter',hoverEffectOn);
+share.addEventListener('mouseleave',hoverEffectOff);
+shareButton.forEach(shareSelected);
 
 function shareSelected(item){
     item.addEventListener('click', function(){
-        let shareBlock = document.querySelector('.share-block');
-
         shareBlock.classList.toggle('share-block-visible');
     })
+}
+
+function hoverEffectOn(){
+    if (shareBlock.classList.contains('share-block-visible')) return;
+    share.classList.add('content__wrapper-image-hover');
+    arrow.classList.add('content__share-button-hover');
+}
+
+function hoverEffectOff(){
+    if (shareBlock.classList.contains('share-block-visible')) return;
+    share.classList.remove('content__wrapper-image-hover');
+    arrow.classList.remove('content__share-button-hover');
 }
